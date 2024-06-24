bd8d62d9aa4affbc3a50702826d218a65f185150
prosnake1 <7bogdanqras@gmail.com>
2024-06-24 10:52:55 +0300

    run zone guard
    перенес все функции в run zone guard

--- lib/main.dart
+++ lib/main.dart
@@ -17,22 +17,24 @@ import 'package:talker_dio_logger/talker_dio_logger.dart';
 import 'package:talker_flutter/talker_flutter.dart';
 
 void main() async {
-  final talker = TalkerFlutter.init();
-  GetIt.I.registerSingleton(talker);
-  GetIt.I<Talker>().debug('Talker started...');
-  final dio = Dio();
-  dio.interceptors.add(TalkerDioLogger(
-      settings: const TalkerDioLoggerSettings(printResponseData: false)));
-  Bloc.observer = TalkerBlocObserver(
-      talker: talker,
-      settings: const TalkerBlocLoggerSettings(
-          printStateFullData: false, printEventFullData: false));
-  WidgetsFlutterBinding.ensureInitialized();
-  await Firebase.initializeApp();
-  setup(dio: dio);
-  FlutterError.onError =
-      (details) => GetIt.I<Talker>().handle(details.exception, details.stack);
-  runZonedGuarded(() => runApp(const MovieApp()), (error, stack) {
+  runZonedGuarded(() async {
+    final talker = TalkerFlutter.init();
+    GetIt.I.registerSingleton(talker);
+    GetIt.I<Talker>().debug('Talker started...');
+    final dio = Dio();
+    dio.interceptors.add(TalkerDioLogger(
+        settings: const TalkerDioLoggerSettings(printResponseData: false)));
+    Bloc.observer = TalkerBlocObserver(
+        talker: talker,
+        settings: const TalkerBlocLoggerSettings(
+            printStateFullData: false, printEventFullData: false));
+    WidgetsFlutterBinding.ensureInitialized();
+    await Firebase.initializeApp();
+    setup(dio: dio);
+    FlutterError.onError =
+        (details) => GetIt.I<Talker>().handle(details.exception, details.stack);
+    runApp(const MovieApp());
+  }, (error, stack) {
     GetIt.I<Talker>().handle(error, stack);
   });
 }
